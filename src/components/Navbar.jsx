084a4574f836c7dfeb491cb6569bc969cedab2c6
nikhil nair <nikhilnair340@gmail.com>
2022-11-20 19:03:55 +0530

    change in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ export default function Navbar(props) {
       className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}
     >
       <div className="container-fluid">
-        <Link className="navbar-brand" to="/ ">
+        <Link className="navbar-brand" to="/">
           {props.title}
         </Link>
         <button
